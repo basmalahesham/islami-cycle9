08427709c793e38e98bb1a62949b6f00e2aa629d
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2023-11-27 18:07:31 +0200

    initialize App add home screen add quran tab add sura details

--- lib/moduls/radio/radio_view.dart
+++ lib/moduls/radio/radio_view.dart
@@ -1,61 +1,116 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:islami/model/RadioModel.dart';
+import 'package:islami/moduls/radio/radio_service.dart';
 
-class RadioView extends StatelessWidget {
+class RadioView extends StatefulWidget {
   const RadioView({super.key});
+
+  @override
+  State<RadioView> createState() => _RadioViewState();
+}
+
+class _RadioViewState extends State<RadioView> {
+  final player = AudioPlayer();
+  int index = 0;
+  bool isPlaying = false;
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Center(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 150,
-          ),
-          Image.asset('assets/images/radio_image.png'),
-          SizedBox(
-            height: 25,
-          ),
-          Text(
-            'إذاعة القرآن الكريم',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      child: FutureBuilder<RadioModel>(
+        future: getRadio(),
+        builder: (context, snapshot) {
+          var data = snapshot.data?.radios ?? [];
+          return Column(
             children: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.skip_previous,
-                  size: 40,
-                  color: theme.canvasColor,
+              SizedBox(
+                height: 150,
+              ),
+              Image.asset('assets/images/radio_image.png'),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                data[index].name ?? 'إذاعة القرآن الكريم',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.play_arrow,
-                  size: 40,
-                  color: theme.canvasColor,
-                ),
+              SizedBox(
+                height: 25,
               ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.skip_next,
-                  size: 40,
-                  color: theme.canvasColor,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      if (index > 0) {
+                        index--;
+                        player.stop();
+                        player.play(UrlSource(data[index].url ?? ''));
+                        setState(() {});
+                      }
+                    },
+                    icon: Icon(
+                      Icons.skip_previous,
+                      size: 40,
+                      color: theme.canvasColor,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      player.stop();
+                      if (!isPlaying) {
+                        player.play(UrlSource(data[index].url ?? ''));
+                      }
+                      //player.play(UrlSource(data[index].url ?? ''));
+                      isPlaying = !isPlaying;
+                      setState(() {});
+                    },
+                    icon: Icon(
+                      isPlaying ? Icons.stop : Icons.play_arrow,
+                      //Icons.play_arrow,
+                      size: 40,
+                      color: theme.canvasColor,
+                    ),
+                  ),
+                  /*IconButton(
+                    onPressed: () {
+                      player.stop();
+                      isPlaying = false;
+                      setState(() {
+
+                      });
+                    },
+                    icon: Icon(
+                      Icons.stop,
+                      size: 40,
+                      color: theme.canvasColor,
+                    ),
+                  ),*/
+                  IconButton(
+                    onPressed: () {
+                      if (index < data.length) {
+                        index++;
+                        player.stop();
+                        player.play(UrlSource(data[index].url ?? ''));
+                        setState(() {});
+                      }
+                    },
+                    icon: Icon(
+                      Icons.skip_next,
+                      size: 40,
+                      color: theme.canvasColor,
+                    ),
+                  ),
+                ],
               ),
             ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
