cd5e3faa0ae0a60556e0737c32765a2561bf8f72
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2023-12-21 20:35:39 +0200

    initialize App add home screen add quran tab add sura details

--- lib/moduls/radio/radio_view.dart
+++ lib/moduls/radio/radio_view.dart
@@ -32,52 +32,155 @@ class _RadioViewState extends State<RadioView> {
               SizedBox(
                 height: 25,
               ),
-              Text(
-                data[index].name ?? 'إذاعة القرآن الكريم',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w600,
+              if (data != null &&
+                  data.isNotEmpty &&
+                  index <
+                      data.length) // Check if data is not null and not empty
+                Column(
+                  children: [
+                    Text(
+                      data[index].name ?? 'إذاعة القرآن الكريم',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            if (index > 0) {
+                              index--;
+                              player.stop();
+                              player.play(UrlSource(data[index].url ?? ''));
+                              setState(() {});
+                            }
+                          },
+                          icon: Icon(
+                            Icons.skip_previous,
+                            size: 40,
+                            color: theme.canvasColor,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            player.stop();
+                            if (!isPlaying) {
+                              player.play(UrlSource(data[index].url ?? ''));
+                            }
+                            //player.play(UrlSource(data[index].url ?? ''));
+                            isPlaying = !isPlaying;
+                            setState(() {});
+                          },
+                          icon: Icon(
+                            isPlaying ? Icons.stop : Icons.play_arrow,
+                            //Icons.play_arrow,
+                            size: 40,
+                            color: theme.canvasColor,
+                          ),
+                        ),
+                        /*IconButton(
+                onPressed: () {
+                  player.stop();
+                  isPlaying = false;
+                  setState(() {
+
+                  });
+                },
+                icon: Icon(
+                  Icons.stop,
+                  size: 40,
+                  color: theme.canvasColor,
                 ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      if (index > 0) {
-                        index--;
-                        player.stop();
-                        player.play(UrlSource(data[index].url ?? ''));
-                        setState(() {});
-                      }
-                    },
-                    icon: Icon(
-                      Icons.skip_previous,
-                      size: 40,
-                      color: theme.canvasColor,
+              ),*/
+                        IconButton(
+                          onPressed: () {
+                            if (index < data.length - 1) {
+                              // Check if index is within the valid range
+                              index++;
+                              player.stop();
+                              player.play(UrlSource(data[index].url ?? ''));
+                              setState(() {});
+                            }
+                          },
+                          icon: Icon(
+                            Icons.skip_next,
+                            size: 40,
+                            color: theme.canvasColor,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      player.stop();
-                      if (!isPlaying) {
-                        player.play(UrlSource(data[index].url ?? ''));
-                      }
-                      //player.play(UrlSource(data[index].url ?? ''));
-                      isPlaying = !isPlaying;
-                      setState(() {});
-                    },
-                    icon: Icon(
-                      isPlaying ? Icons.stop : Icons.play_arrow,
-                      //Icons.play_arrow,
-                      size: 40,
-                      color: theme.canvasColor,
-                    ),
-                  ),
-                  /*IconButton(
+                  ],
+                ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+/*
+Column(
+children: [
+SizedBox(
+height: 150,
+),
+Image.asset('assets/images/radio_image.png'),
+SizedBox(
+height: 25,
+),
+Text(
+data[index].name ?? 'إذاعة القرآن الكريم',
+style: TextStyle(
+fontSize: 25,
+fontWeight: FontWeight.w600,
+),
+),
+SizedBox(
+height: 25,
+),
+Row(
+mainAxisAlignment: MainAxisAlignment.center,
+children: [
+IconButton(
+onPressed: () {
+if (index > 0) {
+index--;
+player.stop();
+player.play(UrlSource(data[index].url ?? ''));
+setState(() {});
+}
+},
+icon: Icon(
+Icons.skip_previous,
+size: 40,
+color: theme.canvasColor,
+),
+),
+IconButton(
+onPressed: () {
+player.stop();
+if (!isPlaying) {
+player.play(UrlSource(data[index].url ?? ''));
+}
+//player.play(UrlSource(data[index].url ?? ''));
+isPlaying = !isPlaying;
+setState(() {});
+},
+icon: Icon(
+isPlaying ? Icons.stop : Icons.play_arrow,
+//Icons.play_arrow,
+size: 40,
+color: theme.canvasColor,
+),
+),
+*/
+/*IconButton(
                     onPressed: () {
                       player.stop();
                       isPlaying = false;
@@ -90,28 +193,24 @@ class _RadioViewState extends State<RadioView> {
                       size: 40,
                       color: theme.canvasColor,
                     ),
-                  ),*/
-                  IconButton(
-                    onPressed: () {
-                      if (index < data.length) {
-                        index++;
-                        player.stop();
-                        player.play(UrlSource(data[index].url ?? ''));
-                        setState(() {});
-                      }
-                    },
-                    icon: Icon(
-                      Icons.skip_next,
-                      size: 40,
-                      color: theme.canvasColor,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
+                  ),*/ /*
+
+IconButton(
+onPressed: () {
+if (index < data.length) {
+index++;
+player.stop();
+player.play(UrlSource(data[index].url ?? ''));
+setState(() {});
 }
+},
+icon: Icon(
+Icons.skip_next,
+size: 40,
+color: theme.canvasColor,
+),
+),
+],
+),
+],
+)*/
